bdbb60638132e1c151ffd2ce98abfa711db58a13
andresarndor <naiperlit347@gmai.com>
2018-11-22 08:25:08 +0000

    fixed empty object bug

--- manage_cluster.sh
+++ manage_cluster.sh
@@ -1,34 +1,54 @@
 #!/bin/bash
 
-#salt "kvm*" cmd.run "ip link add br0 type bridge"
-#salt "kvm*" cmd.run "ip link add br1 type bridge"
+# Script for managing control plane VMs
 
 option1=$1
 option2=$2
 option3=$3
-backup_name=1
+
+function helper () {
+echo ""
+printf "Example: ./manage_cluster.shs vm destroy 1 "
+printf "issues a hard shutdown to all VMs on KVM1 "
+echo "except cfg01 "
+printf "You can use the following commands as an option one: "
+printf "vm, to manage virtual machines, you should pass standard "
+printf "virsh commands as a second option to it, "
+printf "backup, to manage images backups, you should pass "
+echo "create, list, restore, and delete commands as a second option"
+printf "You should use 1, 2, or 3 as a third options indicating "
+printf "a specific KVM node to run the command on, leaving "
+echo "the third option empty will perform the action on all KVMs"
+echo ""
+}
 
 function manage_kvm () {
-	if [[ $option3 == "list" ]]; then
-		salt "kvm*" cmd.run \
+	if [[ $option2 == "list" ]]; then
+		salt "kvm0${option3}.test.local" cmd.run \
 		"
 			virsh list --all
 		"
-		exit
+	else
+		if [[ $option2 == "reboot" ]] || [[ $option2 == "destroy" ]] ||\
+				[[ $option2 == "undefine" ]]; then
+			timeout=1;
+		else
+			timeout=40;
+		fi
+		salt "kvm0${option3}.test.local" cmd.run \
+		"
+			virsh $option2 ctl0${option3}.test.local && sleep ${timeout};
+			virsh $option2 msg0${option3}.test.local && sleep ${timeout};
+			virsh $option2 nal0${option3}.test.local && sleep ${timeout};
+			virsh $option2 dbs0${option3}.test.local && sleep ${timeout};
+			virsh $option2 log0${option3}.test.local && sleep ${timeout};
+			virsh $option2 mon0${option3}.test.local && sleep ${timeout};
+			virsh $option2 mtr0${option3}.test.local && sleep ${timeout};
+			virsh $option2 ntw0${option3}.test.local && sleep ${timeout};
+			virsh $option2 prx0$(( option3-1 )).test.local;
+			virsh list --all
+		"
 	fi
-	salt "kvm0${option3}.test.local" cmd.run \
-	"
-		virsh $option2 ctl0${option3}.test.local;
-		virsh $option2 msg0${option3}.test.local;
-		virsh $option2 nal0${option3}.test.local;
-		virsh $option2 dbs0${option3}.test.local;
-		virsh $option2 log0${option3}.test.local;
-		virsh $option2 mon0${option3}.test.local;
-		virsh $option2 mtr0${option3}.test.local;
-		virsh $option2 ntw0${option3}.test.local;
-		virsh $option2 prx0$(( option3-1 )).test.local;
-		virsh list --all
-	"
 }
 
 function backup_kvm () {
@@ -38,36 +58,46 @@ function backup_kvm () {
 			ls /var/lib/libvirt/
 		"
 		exit
-	elif [[ $option2 == "create" ]]; then
-		if [[ $backup_name == 1 ]]; then
-			printf "Enter backup name: "
-			read backup_name
-			echo ""
-		fi
+	fi
+
+	if [[ $option2 == "create" ]]; then
 		salt "kvm0${option3}.test.local" cmd.run \
-			"
-				test ! -d /var/lib/libvirt/backup_${backup_name} && \
-				mkdir /var/lib/libvirt/backup_${backup_name};
-				cp -r /var/lib/libvirt/images/*local \
-				/var/lib/libvirt/backup_${backup_name}/
-			"
+		"
+			test ! -d /var/lib/libvirt/backup_${backup_name} && \
+			mkdir /var/lib/libvirt/backup_${backup_name};
+			cp -r /var/lib/libvirt/images/*local \
+			/var/lib/libvirt/backup_${backup_name}/
+		"
 	elif [[ $option2 == "restore" ]]; then
-		printf "Enter backup name: "
-		read backup_name
-		echo ""
 		salt "kvm0${option3}.test.local" cmd.run \
-			"
-				test /var/lib/libvirt/backup_${backup_name} && \
-				cp /var/lib/libvirt/backup_${backup_name}/*.local \
-				/var/lib/libvirt/images/
-			"
+		"
+			test /var/lib/libvirt/backup_${backup_name} && \
+			cp /var/lib/libvirt/backup_${backup_name}/*.local \
+			/var/lib/libvirt/images/
+		"
+	elif [[ $option2 == "delete" ]]; then
+		printf "Are you you sure you wanna delete ${backup_name} y/n: "
+		read sure
+			if [[ $sure == "y" ]]; then
+				salt "kvm0*" cmd.run \
+				"
+					rm -r /var/lib/libvirt/backup_${backup_name}
+				"
+			elif [[ $sure == "n" ]];then
+				exit
+		fi
 	fi
 }
 
 if [[ $option1 == "backup" ]]; then
+	if [[ $option2 == "create" ]] || [[ $option2 == "delete" ]]; then
+		printf "Enter backup name: "
+		read backup_name
+		echo ""
+	fi
 	if [[ $option3 == "" ]]; then
-		for kvm in {1..3}; do
 			option3=1
+		for kvm in {1..3}; do
 			backup_kvm
 			echo VM Images on KVM0${option3} have been backed up
 			let option3++
@@ -78,16 +108,20 @@ if [[ $option1 == "backup" ]]; then
 	fi
 elif [[ $option1 == "vm" ]]; then
 	if [[ $option3 == "" ]]; then
+		option3=1
 		for kvm in {1..3}; do
-			option3=1
 			manage_kvm
-			echo $option2 Virtual Machines on KVM0${option3}
+			echo ${option2}ed Virtual Machines on KVM0${option3}
 			let option3++
 		done
 	else
 		manage_kvm
-		echo $option2 Virtual Machines on KVM0${option3}
+		echo ${option2}ed Virtual Machines on KVM0${option3}
 	fi
+elif [[ $option1 == "help" ]] || [[ $option1 == "--help" ]]; then
+	helper
+else
+	echo "'$option1' is a wrong command, use 'backup', 'vm' or 'help'"
 fi
 
 exit
